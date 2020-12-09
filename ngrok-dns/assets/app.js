cb3053bedf8ba98c739abfe1346b1e7c487ebc36
june07 <667@onezerohosting.com>
2020-12-09 05:13:26 +0000

    .

--- ngrok-dns/assets/app.js
+++ ngrok-dns/assets/app.js
@@ -3,7 +3,7 @@ let http = require('http'),
     ngrokDNS = require('@667/ngrok-dns');
 
 http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' })
+    response.writeHead(200, { 'Content-Type': 'text/html' })
     response.end(`Hello from @667/ngrok-dns! Cheers to many debugging sessions over your fixed enpoint! 🍻`)
 }).listen(8080);
 
