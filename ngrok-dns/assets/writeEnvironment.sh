e57a93aec943071811b378179b528ce09028718e
june07 <667@onezerohosting.com>
2020-12-09 03:45:47 +0000

    .

--- ngrok-dns/assets/writeEnvironment.sh
+++ ngrok-dns/assets/writeEnvironment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo REDIRECT_SERVICE_USERID=$(ls .REDIRECT_SERVICE_USERID* | sed s@___@/@g | awk 'BEGIN {FS="PREDIRECT_SERVICE_APIKEY"}{print $1}' | awk 'BEGIN {FS=".REDIRECT_SERVICE_USERID"}{print $2}') > .env
+echo export REDIRECT_SERVICE_USERID=$(ls .REDIRECT_SERVICE_USERID* | sed s@___@/@g | awk 'BEGIN {FS="PREDIRECT_SERVICE_APIKEY"}{print $1}' | awk 'BEGIN {FS=".REDIRECT_SERVICE_USERID"}{print $2}') > .bashrc
 
-echo REDIRECT_SERVICE_APIKEY=$(ls .REDIRECT_SERVICE_USERID* | sed s@___@/@g | awk 'BEGIN {FS="PREDIRECT_SERVICE_APIKEY"}{print $2}') >> .env
+echo export REDIRECT_SERVICE_APIKEY=$(ls .REDIRECT_SERVICE_USERID* | sed s@___@/@g | awk 'BEGIN {FS="PREDIRECT_SERVICE_APIKEY"}{print $2}') >> .bashrc
 
 rm .REDIRECT_SERVICE_USERID*
